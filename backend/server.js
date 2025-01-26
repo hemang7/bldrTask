e0f1dd2c8c68261b8089a7618456d65c44c6b8fc
Hemang <hemang2050@gmail.com>
2025-01-26 04:35:42 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Your React app's URL
+        origin: "https://bldr-task.vercel.app/", // Your React app's URL
         methods: ["GET", "POST"]
     }
 });
