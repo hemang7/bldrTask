7e41d69465227f2a111ee9bdf56f3bc46ead21a9
Hemang <hemang2050@gmail.com>
2025-01-26 04:46:22 -0500

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://bldrtask.onrender.com"); // Update this to your backend URL if different
+const socket = io("https://bldrtask.onrender.com"); // backend app's url
 export default socket;
