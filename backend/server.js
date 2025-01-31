a31c800c96337892992738cc2f41904ad62406a3
Hemang <hemang2050@gmail.com>
2025-01-31 03:58:56 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,10 @@ io.on("connection", (socket) => {
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../build/index.html'));
+    });
+
 
 server.listen(9000, () => {
     console.log('Server is running on port 9000');
